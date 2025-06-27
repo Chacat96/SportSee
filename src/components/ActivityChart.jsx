5ac11e783c9fb15a74803893b5b748b5ba49c181
Charlene CATHERIN <c.catherin@outlook.fr>
2025-06-27 09:29:38 +0200

    chart activity ok

---
+++ src/components/ActivityChart.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    LineChart,
+    Line,
+    ResponsiveContainer
+
+  } from "recharts";
+import '../styles/css/ActivityChart.css';
+import TooltipsActivity from "./TooltipsActivity";
+
+
+function ActivityChart ({data}) {
+    return (
+        <>
+        <div className="activityChart">
+            <div className="activityChart__title">
+                <h2>Activité quotidienne</h2>
+                <div className="activityChart__title__legend">
+                    <span className="activityChart__title__legend__colorBlack"></span><p>Poids (kg)</p>
+                    <span className="activityChart__title__legend__colorRed"></span><p>Calories brûlées (kCal)</p>
+                </div>
+            </div>
+            
+            <BarChart width={900} height={250} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="day" tickFormatter={(_, index) => index + 1} />
+            <YAxis orientation="right" axisLine={false}/>
+            <Tooltip content={<TooltipsActivity/>}           
+            />
+            <Bar dataKey="kilogram" fill="#dark" radius={[10, 10, 0, 0]}/>
+            <Bar dataKey="calories" fill="#FF0000" radius={[10, 10, 0, 0]}/>
+            </BarChart>
+        </div>
+        </>
+    )
+}
+
+export default ActivityChart;
